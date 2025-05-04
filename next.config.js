1e4ba3ca8213869349745e9adf6baad4b56e8865
Hiro04526 <21hiro44@gmail.com>
2025-05-04 23:25:33 +0800

    config-fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: '/Hiro-Portfolio',     
+  assetPrefix: '/Hiro-Portfolio',
   eslint: {
     ignoreDuringBuilds: true,
   },
